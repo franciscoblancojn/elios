83486ae32601b1fffce37350af0a9f0fb2790f96
franciscoblancojn <blancofrancisco34@gmail.com>
2021-09-02 13:56:42 -0500

    maintenance

---
+++ components/maintenance/index.jsx
@@ -0,0 +1,10 @@
+const Maintenance = () => {
+    return (
+        <div className="maintenance">
+            <img src="/img/elios_logo_h.png" alt="Elios" className="imgLogo"/>
+            <br />
+            <h1>We are in maintenance</h1>
+        </div>
+    )
+}
+export default Maintenance
--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,10 @@ import '../styles/global.css'
 import 'react-date-range/dist/styles.css'; // main css file
 import 'react-date-range/dist/theme/default.css';
 
+import Maintenance from '@/components/maintenance'
+
 function MyApp({ Component, pageProps }) {
+  return <Maintenance/>
   return <Component {...pageProps} />
 }
 export default MyApp
