ecdd08d3c2ec4b36951c4fab6ba0d791f37b5d80
franciscoblancojn <blancofrancisco34@gmail.com>
2021-09-03 11:51:16 -0500

    loaderCircle style

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react"
 
+import Islogin from "@/components/isLogin";
 import Content from "@/components/content";
 import TitelIcon from "@/components/title/titleIcon";
 import ListIntegracion from "@/components/list/integracion";
@@ -29,26 +30,28 @@ class Index extends React.Component {
     }
     render() {
         return (
-            <Content 
-            title="Elios"
-            className="cMenu home"
-            >
-                <TitelIcon
-                title={this.state.title}
-                subtitle={(
-                    <>
-                        {this.state.text1}
-                        <br/>
-                        {this.state.text2}
-                    </>
-                )}
-                icon={(<img src="/img/elios.png" alt="Elios" />)}
-                />
-                <ListIntegracion
-                title={this.state.titleList}
-                btn={this.state.btn}
-                />
-            </Content>
+            <Islogin>
+                <Content 
+                title="Elios"
+                className="cMenu home"
+                >
+                    <TitelIcon
+                    title={this.state.title}
+                    subtitle={(
+                        <>
+                            {this.state.text1}
+                            <br/>
+                            {this.state.text2}
+                        </>
+                    )}
+                    icon={(<img src="/img/elios.png" alt="Elios" />)}
+                    />
+                    <ListIntegracion
+                    title={this.state.titleList}
+                    btn={this.state.btn}
+                    />
+                </Content>
+            </Islogin>
         )
     }
 }
