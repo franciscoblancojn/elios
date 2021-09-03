5ff78a5f32b56596035c5a2d1e8929ecaa5e8643
franciscoblancojn <blancofrancisco34@gmail.com>
2021-09-03 16:46:51 -0500

    validate site in visitantes y single

--- pages/visitantes/[id].jsx
+++ pages/visitantes/[id].jsx
@@ -1,17 +1,23 @@
 import React, { Component } from "react"
 
+import Islogin from "@/components/checkLogin/isLogin";
+import ExistOneSite from "@/components/checkSite/existOneSite";
 import Content from "@/components/content";
 import SingleVisitantes from "@/components/table/single/visitantes";
 
 
 const Index = ({id}) => {
     return (
-        <Content 
-        title="Visitantes"
-        className="cMenu page-visitantes"
-        >
-            <SingleVisitantes id={id}></SingleVisitantes>
-        </Content>
+        <Islogin>
+            <Content 
+            title="Visitantes"
+            className="cMenu page-visitantes"
+            >
+                <ExistOneSite>
+                    <SingleVisitantes id={id}></SingleVisitantes>
+                </ExistOneSite>
+            </Content>
+        </Islogin>
     )
 }
 
