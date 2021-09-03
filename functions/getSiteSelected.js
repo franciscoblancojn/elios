a1574db93f7e606eb4b12250b90804a7993d9ec9
franciscoblancojn <blancofrancisco34@gmail.com>
2021-09-03 18:11:05 -0500

    fixed get siteselected

--- functions/getSiteSelected.js
+++ functions/getSiteSelected.js
@@ -3,10 +3,12 @@ import {getUser} from "@/functions/index";
 const getSiteSelected = () => {
     const user = getUser()
     const sites = user.sites || []
-    if(!localStorage.getItem('site')){
-        return sites[0]
-    }else{
-        return JSON.parse(localStorage.getItem('site'))
+    if(localStorage.getItem('site')){
+        const site = JSON.parse(localStorage.getItem('site'))
+        if(sites.find((e)=>e.host==site.host)){
+            return site
+        }
     }
+    return sites[0]
 }
 export default getSiteSelected
