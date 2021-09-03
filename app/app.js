b4506b64510239ee4e197140d13a9795bd2517df
franciscoblancojn <blancofrancisco34@gmail.com>
2021-09-02 20:47:56 -0500

    updateUser

--- app/app.js
+++ app/app.js
@@ -4,28 +4,8 @@ export const CreateAccount = a_CreateAccount
 import a_getLeads from '@/app/getLeads'
 export const getLeads = a_getLeads
 
-export const PutQuestionnaire = ({uid,email,questionnaire}, respondOk = (e) => console.log(e), respondError = (e) => console.log(e)) => {
-    request(
-        "putQuestionnaire",
-        {
-            uid,
-            email,
-            questionnaire
-        },
-        respondOk,
-        respondError
-    )
-}
-export const getClientes = ({key = "", host = "", query = {}, _return = {} , respondOk = (e) => console.log(e), respondError = (e) => console.log(e)}) => {
-    request(
-        "getClientes",
-        {
-            key,
-            host,
-            query,
-            return : _return
-        },
-        respondOk,
-        respondError
-    )
-}
+import a_getClients from '@/app/getClients'
+export const getClients = a_getClients
+
+import a_updateUser from '@/app/updateUser'
+export const updateUser = a_updateUser
