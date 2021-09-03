c971deea47bf0d90a8bfe71c4bb0f50f163b7054
franciscoblancojn <blancofrancisco34@gmail.com>
2021-09-03 15:40:23 -0500

    btn sites lang

--- components/btn/btnSites.jsx
+++ components/btn/btnSites.jsx
@@ -1,6 +1,4 @@
-import React, { Component } from "react"
 import { useState, useEffect } from "react"
-import { useRouter } from "next/router"
 import Link from 'next/link'
 
 import SvgArrow from "@/components/svg/arrow"
@@ -8,154 +6,8 @@ import SvgSearch from "@/components/svg/search"
 
 import {getUser,getSiteSelected,setSiteSelected} from "@/functions/index";
 
-class BtnSites2 extends React.Component {
-    state = {
-        style:false,
-        hidden:false,
-        site:[],
-        siteShow:[],
-        icon:{},
-        select:"",
-        searchSite:""
-    }
-    styleUl = () => {
-        const ulSites = document.getElementById("ulSites")
-        const {
-            left,
-            top,
-            width,
-            height
-        } = ulSites.getBoundingClientRect()
+import languajes from "@/languajes/languaje";
 
-        ulSites.style.width = `${width}px`
-        ulSites.style.height = `${height}px`
-        ulSites.style.top = `${top}px`
-        ulSites.style.left = `${left}px`
-        ulSites.style.opacity = "1"
-        ulSites.style.zIndex = "9999"
-        ulSites.style.position = "fixed"
-        ulSites.style.minWidth = "0"
-        ulSites.style.maxHeight = `calc(100vh - ${top}px)`
-
-        this.setState({
-            style : true
-        })
-    }
-    componentDidMount(){
-        const currentUser =  this.props.currentUser
-        const site = currentUser?.host ?? []
-        const siteShow = currentUser?.host ?? []
-        const icon = currentUser?.cms ?? {}
-        var select = localStorage.getItem('hostSelect') ?? site[0]
-        if(!site.includes(select)){
-            select = site[0]
-            localStorage.setItem('hostSelect',select)
-        }
-        this.setState({
-            site,
-            siteShow,
-            icon,
-            select
-        })
-
-        this.onChangeSite = ((e) => {
-            this.setState({select:e})
-            this.props.onChange(e)
-        }) ?? ((e) => {})
-        getUser()
-    }
-    onChangeSite = () => {}
-    toggleHidden = () => {
-        if(!this.state.style){
-            this.styleUl()
-        }else{
-            this.setState({hidden:!this.state.hidden})
-        }
-    }
-    onChangeSearchSite = (e) => {
-        const value = e.target.value
-        const siteShow = this.state.site.filter((e)=>e.indexOf(value)>-1)
-        this.setState({
-            searchSite : value,
-            siteShow
-        })
-    }
-    render() {
-        const site = this.state.site ?? []
-        const siteShow = this.state.siteShow ?? []
-        const icon = this.state.icon ?? {}
-        const select = this.state.select ?? site[0]
-        return (
-            <>
-                {
-                    (site.length == 0)?
-                    (
-                        <Link href="/">
-                            <a 
-                            className={`btn-2 ${this.props.className || ""}`}
-                            >
-                                <img src="/icons/+.svg" alt="+" className="iconM"/>
-                                AGREGAR SITIO NUEVO
-                            </a>
-                        </Link>
-                    )
-                    :
-                    (
-                        <div className={`btn-select ${this.props.className || ""}`}>
-                            <button
-                            className="btn-2"
-                            onClick={this.toggleHidden}
-                            >
-                                <span className="select">
-                                    <img src={`/icons/icon-${icon[select]}.png`} alt={select} className="iconHost"/>
-                                    {select}
-                                    <SvgArrow/>
-                                </span>
-                            </button>
-                            <ul hidden={this.state.hidden} id="ulSites">
-                                <li>
-                                    <div className="search">
-                                        <SvgSearch></SvgSearch>
-                                        <input type="text" name="searchSite" id="searchSite" onChange={this.onChangeSearchSite} value={this.state.searchSite} placeholder="Buscar Sitio"/>
-                                    </div>
-                                </li>
-                                <li>
-                                    <Link href="/">
-                                        <a 
-                                        className={`btn ${this.props.className || ""}`}
-                                        >
-                                            AGREGAR SITIO NUEVO
-                                        </a>
-                                    </Link>
-                                </li>
-                                {
-                                    siteShow.length > 0 ?
-                                    siteShow.map((e,i)=>(
-                                        <li key={i} value={e}
-                                        onClick={
-                                            ()=>{
-                                                this.onChangeSite(e)
-                                                this.toggleHidden()
-                                            }
-                                        }
-                                        >
-                                            <img src={`/icons/icon-${icon[e]}.png`} alt={e} className="iconHost"/>
-                                            {e}
-                                        </li>
-                                    ))
-                                    :
-                                    (
-                                        <li className="noFound">No found</li>
-                                    )
-                                }
-                            </ul>
-                        </div>
-                    )
-                }
-            </>
-        )
-    }
-}
 const BtnSites = ({className}) => {
     const user = getUser()
     const sites = user.sites || []
@@ -164,6 +16,18 @@ const BtnSites = ({className}) => {
     const [showList, setShowList] = useState(true)
     const [siteShow, setSiteShow] = useState(sites)
     const [textSearch, setTextSearch] = useState("")
+
+    const [allText, setAllText] = useState({
+        addNewSite:"AGREGAR SITIO NUEVO",
+        noFound: "No hay resultados"
+    })
+    const loadLanguajes = async () => {
+        const lang = await languajes()
+        setAllText({
+            ...allText,
+            ...lang.btnSites
+        })
+    }
 
     const toggleShowList = () => {
         setShowList(!showList)
@@ -177,6 +41,10 @@ const BtnSites = ({className}) => {
         setTextSearch(value)
         setSiteShow(sites.filter((e)=>e.host.indexOf(value)>-1))
     }
+
+    useEffect(() => {
+        loadLanguajes()
+    }, [])
     return <>
         {
             (sites.length == 0)?
@@ -186,7 +54,7 @@ const BtnSites = ({className}) => {
                     className={`btn-2 ${className || ""}`}
                     >
                         <img src="/icons/+.svg" alt="+" className="iconM"/>
-                        AGREGAR SITIO NUEVO
+                        {allText.addNewSite}
                     </a>
                 </Link>
             )
@@ -215,7 +83,7 @@ const BtnSites = ({className}) => {
                                 <a 
                                 className={`btn ${className|| ""}`}
                                 >
-                                    AGREGAR SITIO NUEVO
+                                    {allText.addNewSite}
                                 </a>
                             </Link>
                         </li>
@@ -236,7 +104,7 @@ const BtnSites = ({className}) => {
                             ))
                             :
                             (
-                                <li className="noFound">No found</li>
+                                <li className="noFound">{allText.noFound}</li>
                             )
                         }
                     </ul>
