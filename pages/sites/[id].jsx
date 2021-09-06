691a2cef828b7473dd752db488ad95d8831fd097
franciscoblancojn <blancofrancisco34@gmail.com>
2021-09-06 18:48:31 -0500

    init sites

---
+++ pages/sites/[id].jsx
@@ -0,0 +1,27 @@
+import React, { Component } from "react"
+
+import Islogin from "@/components/checkLogin/isLogin";
+import ExistOneSite from "@/components/checkSite/existOneSite";
+import Content from "@/components/content";
+import SingleLeads from "@/components/table/single/leads";
+
+
+const Index = ({id}) => {
+    return (
+        <Islogin>
+            <Content 
+            title="Leads"
+            className="cMenu page-leads"
+            >
+                <ExistOneSite>
+                    {id}
+                </ExistOneSite>
+            </Content>
+        </Islogin>
+    )
+}
+
+export async function getServerSideProps({ params }) {
+    return { props: { id : params.id } }
+}
+export default Index
