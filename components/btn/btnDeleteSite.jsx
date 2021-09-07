90fa0fd8c6050f51b034998836043b7d54536c3e
franciscoblancojn <blancofrancisco34@gmail.com>
2021-09-07 00:39:33 -0500

    btnDeleteSite delete site and redirect

--- components/btn/btnDeleteSite.jsx
+++ components/btn/btnDeleteSite.jsx
@@ -1,23 +1,34 @@
 import { useState, useEffect } from "react"
+import { useRouter } from "next/router"
 
 import {deleteSite} from "@/app/app"
 
 import Modal from '@/components/modal/modal'
+import LoaderCircle from "@/components/loader/circle";
 
 const BtnDeleteSite = ({site}) => {
+    const router = useRouter()
     const [showModal, setShowModal] = useState(false)
+    const [loader, setLoader] = useState(false)
+    const deleteSiteClick = async () => {
+        setLoader(true)
+        await deleteSite(site)
+        router.push("/")
+    }
     return <>
+        {loader ? <LoaderCircle/> :
         <div className="btn-delete-site">
             <Modal open={showModal} btn={(<><img src="/icons/delete.svg" alt="delete" />Eliminar Sitio</>)} closeModal={()=>{setShowModal(false)}} toggelModal={()=>{setShowModal(!showModal)}}>
                 <div className="verifit-delete">
                     <h1>Estas completamente seguro</h1>
                     <div className="content-button">
-                        <button className="delete" onClick={()=>{deleteSite(site)}}>Si</button>
+                        <button className="delete" onClick={deleteSiteClick}>Si</button>
                         <button onClick={()=>{setShowModal(false)}}>No</button>
                     </div>
                 </div>
             </Modal>
         </div>
+        }
     </>
 }
 export default BtnDeleteSite
