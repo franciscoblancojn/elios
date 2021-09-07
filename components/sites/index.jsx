936df6f460c4ee30ddd0280ffc085914f9a514fe
franciscoblancojn <blancofrancisco34@gmail.com>
2021-09-06 23:22:14 -0500

    change site

--- components/sites/index.jsx
+++ components/sites/index.jsx
@@ -10,43 +10,31 @@ import FilterDateShow from "@/components/filters/dateShow"
 import ArrowRight from "@/components/svg/right-arrow"
 
 const Site = ({host}) => {
-    const [content, setContent] = useState(<LoaderCircle/>)
-    const loadContent = async () => {
-        const user = getUser()
-        const sites = user.sites || []
-        const site = sites.find((element)=>element.host=host)
-        setContent(
-            <div className="">
-                <div className="flex flex-between flex-align-center">
-                    <Link href={`/`}>
-                        <a className="before mb-0">
-                            <ArrowRight/>
-                            Volver al Inicio
-                        </a>
-                    </Link>
-                    <UserInLive/>
-                </div>
-                <h1 className="title-1">
-                    Hola {site.host}
-                </h1>
-                <div className="container">
-                    <div className="row">
-                        <div className="col-6">
+    return <div className="container" style={{maxWidth:"1000px"}}>
+        <div className="flex flex-between flex-align-center">
+            <Link href={`/`}>
+                <a className="before mb-0">
+                    <ArrowRight/>
+                    Volver al Inicio
+                </a>
+            </Link>
+            <UserInLive/>
+        </div>
+        <h1 className="title-1">
+            Hola {host}
+        </h1>
+        <div className="container">
+            <div className="row">
+                <div className="col-6">
 
-                        </div>
-                        <div className="col-6">
-                            <FilterDateShow/>
-                        </div>
-                    </div>
                 </div>
-                {JSON.stringify(site)}
+                <div className="col-6">
+                    <FilterDateShow onChange={console.log}/>
+                </div>
             </div>
-        )
-    }
-    useEffect( async () => {
-        loadContent()
-    }, [])
-    return <>{content}</>
+        </div>
+        {JSON.stringify(host)}
+    </div>
 }
 
 export default Site
