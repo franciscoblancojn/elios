ff6bfb4496810d927c7bda4670553a26f6af6293
franciscoblancojn <blancofrancisco34@gmail.com>
2021-09-07 19:02:16 -0500

    lang init

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,44 +5,26 @@ import ExistOneSite from "@/components/checkSite/existOneSite";
 import Content from "@/components/content";
 import LoaderCircle from "@/components/sites/list";
 import TitelIcon from "@/components/title/titleIcon";
-import languajes from "@/languajes/languaje";
+import Lang from "@/components/lang/lang";
 
 class Index extends React.Component {
-    state = {
-        title : "Bienvenido a elios!",
-        text1 : "Por favor comienza conectando",
-        text2 : "tu tienda virtual o sitio web.",
-        titleList :"Selecciona la integración:",
-        "btn":{
-            "conectar":"CONECTAR",
-            "proximamente":"PROXIMAMENTE"
-        }
-    }
-    loadLanguajes = async () => {
-        const lang = await languajes()
-        this.setState({
-            ...this.state,
-            ...lang.inicion
-        })
-    }
-    componentDidMount(){
-        this.loadLanguajes()
-    }
     render() {
         return (
             <Islogin>
-                <Content 
-                title="Elios"
-                className="cMenu home"
-                >
-                    <ExistOneSite>
-                        <TitelIcon
-                        title={this.state.title}
-                        icon={(<img src="/img/elios.png" alt="Elios" />)}
-                        />
-                        <LoaderCircle/>
-                    </ExistOneSite>
-                </Content>
+                <Lang>
+                    <Content 
+                    title="Elios"
+                    className="cMenu home"
+                    >
+                        <ExistOneSite>
+                            <TitelIcon
+                            title={JSON.parse(localStorage.getItem('languaje')).inicion.title}
+                            icon={(<img src="/img/elios.png" alt="Elios" />)}
+                            />
+                            <LoaderCircle/>
+                        </ExistOneSite>
+                    </Content>
+                </Lang>
             </Islogin>
         )
     }
