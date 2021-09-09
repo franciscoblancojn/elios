bf61e97b64b17bb0d854ffc1453d0d778d3c8be1
franciscoblancojn <blancofrancisco34@gmail.com>
2021-09-09 10:33:28 -0500

    fixed lang add-site

---
+++ components/page/add-site/index.jsx
@@ -0,0 +1,31 @@
+import Content from "@/components/content";
+import TitelIcon from "@/components/title/titleIcon";
+import ListIntegracion from "@/components/list/integracion";
+
+const AddSite = ({lang}) => {
+    console.log(lang);
+    return (
+        <Content 
+        lang={lang}
+        title="AddSite"
+        className="cMenu home"
+        >
+            <TitelIcon
+            title={lang.addSite.title}
+            subtitle={(
+                <>
+                    {lang.addSite.text1}
+                    <br/>
+                    {lang.addSite.text2}
+                </>
+            )}
+            icon={(<img src="/img/elios.png" alt="Elios" />)}
+            />
+            <ListIntegracion
+            title={lang.addSite.titleList}
+            btn={lang.addSite.btn}
+            />
+        </Content>
+    )
+}
+export default AddSite
