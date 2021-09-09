3d87495dd84d41607c04387e7f33ed2f1424e5b8
franciscoblancojn <blancofrancisco34@gmail.com>
2021-09-09 16:01:16 -0500

    upload img accept

--- components/form/uploadImg.jsx
+++ components/form/uploadImg.jsx
@@ -7,10 +7,10 @@ const UploadImg = ({img,site}) => {
     }
     return (
         <div className="addImg">
-            <form action="http://localhost:3001/api/v2/upload" method="post" encType="multipart/form-data">
-                <input type="hidden" name="jwt" id="jwt" value={JSON.parse(getCookie()).token} />
-                <input type="hidden" name="site" id="site" value={site} />
-                <input type="hidden" name="return" id="return" value={window.location.href} />
+            <form action={`http://localhost:3001/api/v2/upload`} method="post" encType="multipart/form-data">
+                <input type="text" name="jwt" id="jwt" value={JSON.parse(getCookie()).token} />
+                <input type="text" name="site" id="site" value={site} />
+                <input type="text" name="return" id="return" value={window.location.href} />
                 <label htmlFor="addImg">
                     {
                         img ?
@@ -18,7 +18,7 @@ const UploadImg = ({img,site}) => {
                         :
                         <img src="/icons/addImgSite.svg"/>
                     }
-                    <input id="addImg" type="file" accept="image/*" name="photo" onChangeCapture={changeImg}/>
+                    <input id="addImg" type="file" accept="image/png, image/jpeg, image/jpg" name="photo" onChangeCapture={changeImg}/>
                 </label>
                 <input type="submit" value="upload" id="submitAddImg" />
             </form>
