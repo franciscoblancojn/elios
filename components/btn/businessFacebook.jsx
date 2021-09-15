68de0c4429029c9294b5b4c010d65a99c039b117
franciscoblancojn <blancofrancisco34@gmail.com>
2021-09-15 11:02:32 -0500

    accessToken

--- components/btn/businessFacebook.jsx
+++ components/btn/businessFacebook.jsx
@@ -3,6 +3,18 @@ import Head from 'next/head'
 
 
 const BusinessFacebook = ({lang,site}) => {
+    const Connected = () => {
+        FB.init({
+            appId            : process.env.ID_APP_FACEBOOK,
+            version          : 'v12.0'
+        });
+        FB.login(function(response) {
+            if (response.status === 'connected') {
+                var accessToken = response.authResponse.accessToken;
+                console.log(accessToken);
+            } 
+        });
+    }
     return <>
         <Head>
             <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js"></script>
@@ -15,7 +27,7 @@ const BusinessFacebook = ({lang,site}) => {
             :
             <div className="flex"> 
                 <img src="/icons/facebook.png" alt="facebook" style={{marginRight:"5px"}}/>
-                <button className="businessFacebook">
+                <button className="businessFacebook" onClick={Connected}>
                     {lang.sites.conectar} Facebook
                 </button>
             </div>
