40001c1d1f707695b1c7950dc238bbc42ed60c69
franciscoblancojn <blancofrancisco34@gmail.com>
2021-09-30 16:09:36 -0500

    style btnExport

--- components/btn/btnExport.jsx
+++ components/btn/btnExport.jsx
@@ -1,10 +1,13 @@
 import { useState, useEffect } from "react"
 
+import SvgDownload from "@/components/svg/download"
+
 const BtnExpor = ({rows,lang}) => {
     const [showList, setShowList] = useState(false)
     return (
         <>
             <button className="btnExport" onClick={()=>{setShowList(!showList)}}>
+                <SvgDownload/>
                 {lang.btnExport.text}
             </button>
             <div className="listExport" hidden={!showList}>
