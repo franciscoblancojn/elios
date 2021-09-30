1cd37f708519b3ac5e5593118d8095396f26554a
franciscoblancojn <blancofrancisco34@gmail.com>
2021-09-30 16:54:58 -0500

    expor with excel 100%

--- components/btn/btnExport.jsx
+++ components/btn/btnExport.jsx
@@ -1,9 +1,15 @@
 import { useState, useEffect } from "react"
+import exportFromJSON from 'export-from-json'  
 
 import SvgDownload from "@/components/svg/download"
 
 const BtnExpor = ({rows,lang}) => {
     const [showList, setShowList] = useState(false)
+    const exportExcel = () => {
+        setShowList(false)
+        const fileName = (new Date).toISOString().split(" ").join("_")
+        exportFromJSON({ data:rows, fileName, exportType:'xls' }) 
+    }
     return (
         <div className="btnExport">
             <button  onClick={()=>{setShowList(!showList)}}>
@@ -11,7 +17,7 @@ const BtnExpor = ({rows,lang}) => {
                 {lang.btnExport.text}
             </button>
             <div className="listExport" hidden={!showList}>
-                <div className="excel optionExport">
+                <div className="excel optionExport" onClick={exportExcel}>
                     <img src="/icons/excel.png" alt="excel" />
                     {lang.btnExport.excel}
                 </div>
