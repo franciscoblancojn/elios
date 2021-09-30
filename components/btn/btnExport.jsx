a134c9d83b7e873dc329e67a62251ee62ef5b38b
franciscoblancojn <blancofrancisco34@gmail.com>
2021-09-30 16:36:54 -0500

    style listExport

--- components/btn/btnExport.jsx
+++ components/btn/btnExport.jsx
@@ -5,17 +5,18 @@ import SvgDownload from "@/components/svg/download"
 const BtnExpor = ({rows,lang}) => {
     const [showList, setShowList] = useState(false)
     return (
-        <>
-            <button className="btnExport" onClick={()=>{setShowList(!showList)}}>
+        <div className="btnExport">
+            <button  onClick={()=>{setShowList(!showList)}}>
                 <SvgDownload/>
                 {lang.btnExport.text}
             </button>
             <div className="listExport" hidden={!showList}>
-                <div>
+                <div className="excel optionExport">
+                    <img src="/icons/excel.png" alt="excel" />
                     {lang.btnExport.excel}
                 </div>
             </div>
-        </>
+        </div>
     )
 }
 export default BtnExpor
